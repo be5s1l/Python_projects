ecd41d53d91502c4f857b9d4d841a6148c382a33
Ahmed Adel <136595728+be5s1l@users.noreply.github.com>
2026-08-03 16:11:48 +0300

    Refactor auction logic to streamline bidding process and winner determination

--- blind auction/main.py
+++ blind auction/main.py
@@ -1,29 +1,27 @@
 from art import logo
-
 print(logo)
-print("Welcome to the secret auction program.")
 
-bidding = True
-bidding_dict = {
-}
 
-while bidding:
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+
+bids = {}
+continue_bidding = True
+while continue_bidding:
     name = input("What is your name?: ")
-    bid = int(input("What is your bid?: $"))
-    bidding_dict[name] = bid
-    continue_bidding = input("Are there any other bidders? Type 'yes or 'no'.\n").lower()
-    if continue_bidding == "yes":
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
         print("\n" * 20)
-    else:
-        bidding = False
-
-highest_bid = 0
-highest_bidder = ""
-
-for bid in bidding_dict:
-    highest_bidder = bid
-    for bidder in bidding_dict:
-        if bidding_dict[bidder] > highest_bid:
-            highest_bid = bidding_dict[bidder]
-            highest_bidder = bidder
-print(f"The winner is {highest_bidder} with a bid of ${highest_bid}")
